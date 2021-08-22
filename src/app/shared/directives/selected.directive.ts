9a184f4ee32e9cefabeb91aafe6912b35757d906
Salami <salamisodikiolawale@gmail.com>
2021-08-22 16:25:26 +0200

    ..

---
+++ src/app/shared/directives/selected.directive.ts
@@ -0,0 +1,24 @@
+import { Directive, HostBinding, Input, OnChanges } from '@angular/core';
+
+@Directive({
+  selector: '[appSelected]',
+})
+export class SelectedDirective implements OnChanges {
+  @Input() public appSelected!: boolean;
+  @HostBinding('style.backgroundColor') private backgroundColor?: string;
+  @HostBinding('style.fontWeight') private fontWeight?: string;
+  @HostBinding('style.color') private color?: string;
+  constructor() {}
+
+  ngOnChanges(): void {
+    if (this.appSelected) {
+      this.backgroundColor = 'var(--primary)';
+      this.fontWeight = '500';
+      this.color = 'white';
+    } else {
+      this.backgroundColor = 'white';
+      this.fontWeight = '400';
+      this.color = 'var(--text-regular)';
+    }
+  }
+}
