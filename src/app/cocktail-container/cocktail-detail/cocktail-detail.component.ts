b274affdebc713dd21c69266fa76fab205c0fedd
Salami <salamisodikiolawale@gmail.com>
2021-08-22 09:40:49 +0200

    last commit for first version

--- src/app/cocktail-container/cocktail-detail/cocktail-detail.component.ts
+++ src/app/cocktail-container/cocktail-detail/cocktail-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Cocktail } from '../../interfaces/cocktail.interface';
 
 @Component({
@@ -7,12 +7,7 @@ import { Cocktail } from '../../interfaces/cocktail.interface';
   styleUrls: ['./cocktail-detail.component.scss'],
 })
 export class CocktailDetailComponent implements OnInit {
-  public cocktail: Cocktail = {
-    name: 'Le Old Fashioned',
-    img: 'https://maisonfoody.com/sites/default/files/styles/article_paragraph_image/public/2019-11/aquavit-fresh.jpg?itok=C17K5mqQ',
-    description:
-      'Pour la cinquième année classé premier, le Old fashionned se cramponne au sommet ! C’est le roi des cocktails ! Il consiste en un sucre imbibé d’amer sur lequel on verse du whisky.',
-  };
+  @Input() public cocktail!: Cocktail;
 
   constructor() {}
 
